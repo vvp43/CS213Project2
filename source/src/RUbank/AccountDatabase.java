8138d357bdba8fc08d2b20e0454243fe4f52152f
vvp43 <vvp43@scarletmail.rutgers.edu>
2023-10-03 16:15:09 -0400

    Add classes to source

--- source/src/RUbank/AccountDatabase.java
+++ source/src/RUbank/AccountDatabase.java
@@ -9,17 +9,27 @@ public class AccountDatabase {
     private Account[] accounts; //list of various types of accounts
     private int numAcct; //number of accounts in the array
 
-    private int find(Account account) {} //search for an account in the array
+    private int find(Account account) {
+        return 0;
+    } //search for an account in the array
 
     private void grow() {}//increase the capacity by 4
 
-    public boolean contains(Account account){} //overload if necessary
+    public boolean contains(Account account){
+        return false;
+    } //overload if necessary
 
-    public boolean open(Account account){} //add a new account
+    public boolean open(Account account){
+        return false;
+    } //add a new account
 
-    public boolean close(Account account){} //remove the given account
+    public boolean close(Account account){
+        return false;
+    } //remove the given account
 
-    public boolean withdraw(Account account){} //false if insufficient fund
+    public boolean withdraw(Account account){
+        return false;
+    } //false if insufficient fund
 
     public void deposit(Account account){}
 
--- source/src/RUbank/CollegeChecking.java
+++ source/src/RUbank/CollegeChecking.java
@@ -12,4 +12,25 @@ public class CollegeChecking extends Checking {
     //Constants
     final double collegeCheckingFee = 12.0;
     final double collegeCheckingInterestRate = 1.0/100;
+
+    /**
+     * Constructor with campus as parameter
+     * @param campus name fo campus
+     */
+    public CollegeChecking(Campus campus){
+        this.campus=campus;
+    }
+
+    /**
+     * Campus getter method
+     */
+    public Campus getCampus () {
+        return this.campus;
+    }
+
+    public static void main(String[] args) {
+        CollegeChecking cc = new CollegeChecking(Campus.NEW_BRUNSWICK);
+        System.out.println(cc.getCampus());
+
+    }
 }
